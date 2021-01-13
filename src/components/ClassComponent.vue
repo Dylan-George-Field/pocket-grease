b862713e37a454032f58f4a1c7023fbef9a7f8e2
Dylan George Field <dylan.george.field@gmail.com>
2021-01-13 23:41:46 +1100

    Add vue-chart.js

--- src/components/ClassComponent.vue
+++ src/components/ClassComponent.vue
@@ -1,36 +1,14 @@
-<template>
-  <div>
-    <p>{{ title }}</p>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id" @click="increment">
-        {{ todo.id }} - {{ todo.content }}
-      </li>
-    </ul>
-    <p>Count: {{ todoCount }} / {{ meta.totalCount }}</p>
-    <p>Active: {{ active ? 'yes' : 'no' }}</p>
-    <p>Clicks on todos: {{ clickCount }}</p>
-  </div>
-</template>
 
-<script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator'
-import { Todo, Meta } from './models'
 
-@Component
-export default class ClassComponent extends Vue {
-  @Prop({ type: String, required: true }) readonly title!: string;
-  @Prop({ type: Array, default: () => [] }) readonly todos!: Todo[];
-  @Prop({ type: Object, required: true }) readonly meta!: Meta;
-  @Prop(Boolean) readonly active!: boolean;
+<script>
+import { Line } from 'vue-chartjs'
 
-  clickCount = 0;
-
-  increment () {
-    this.clickCount += 1
-  }
-
-  get todoCount () {
-    return this.todos.length
+export default {
+  extends: Line,
+  props: ['chartdata', 'options'],
+  mounted () {
+    console.log('working')
+    this.renderChart(this.chartdata, this.options)
   }
 }
 </script>
