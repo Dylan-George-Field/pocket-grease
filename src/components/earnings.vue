a89d332566a4ba832fa8b27554a5eb37a62f47ce
Dylan George Field <dylan.george.field@gmail.com>
2021-09-21 22:39:32 +1000

    Reduce margins on income/deductions headings

--- src/components/earnings.vue
+++ src/components/earnings.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <h5>Income</h5>
+    <h5 class="q-my-none">Income</h5>
     <q-input v-model="income" label="Yearly Income" type="number" />
     <q-input v-model="startAge" label="Start Age" type=number />
     <q-input v-model="retirementAge" label="Retirement Age" type="number" />
     <q-input v-model="interest" label="Savings Interest %" type="number" />
-    <h5>Deductions</h5>
+    <h5 class="q-mb-none">Deductions</h5>
     <q-input v-model="incomeTax" label="Income Tax %" type="number" />
     <q-input v-model="deductions" label="Yearly Deductions" type="number" />
     <q-btn v-on:click="$emit('submit', { income, deductions, startAge, retirementAge, interest })" color="primary" label="Calculate" class="q-mt-md" />
