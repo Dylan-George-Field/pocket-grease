d9e496055e2efdd2ae970e41a7e21882ab4640ed
Dylan George Field <dylan.george.field@gmail.com>
2021-10-21 23:38:59 +1100

    Make pocket list look neat 🥔

--- src/components/pocket-list.vue
+++ src/components/pocket-list.vue
@@ -2,6 +2,11 @@
   <q-toolbar class="bg-primary text-white shadow-2">
     <q-toolbar-title>Pocket List</q-toolbar-title>
   </q-toolbar>
+  <q-list v-if="tasks.length === 0">
+    <q-item >
+      There are no items yet
+    </q-item>
+  </q-list>
   <q-list v-for="task in tasks" :key="task.name" bordered>
     <q-item clickable>
       {{task.name}}
@@ -23,7 +28,7 @@ export default {
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     const tasks = store.state.graph.tasks as Task[]
-
+    console.log(tasks)
     const deleteTask = function(task: Task): void {
       void store.dispatch('graph/deleteTask', task)
     }
